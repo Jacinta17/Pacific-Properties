899f974690f0340fb34d132f441a2815178c1bdc
jacinta <jacintaferns175@gmail.com>
2025-09-29 14:23:26 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// JS for sidebar toggle
+    const menuBtn = document.getElementById('menuBtn');
+    const sidebar = document.getElementById('sidebar');
+    const overlay = document.getElementById('overlay');
+
+    menuBtn.addEventListener('click', () => {
+      sidebar.classList.toggle('-translate-x-full');
+      overlay.classList.toggle('hidden');
+    });
+
+    overlay.addEventListener('click', () => {
+      sidebar.classList.add('-translate-x-full');
+      overlay.classList.add('hidden');
+    });
